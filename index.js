d8eff64da6ecdd95797fa277cde28bcf3c724f66
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-12-24 12:37:16 +0300

    done

--- index.js
+++ index.js
@@ -13,15 +13,24 @@
 
 
 // fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=ea04db02d64d4b2b6453bfc814cd3cf9`)
-fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=ea04db02d64d4b2b6453bfc814cd3cf9`)
+fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=52,8926&lon=30,024&dt=1586468027&appid=ea04db02d64d4b2b6453bfc814cd3cf9`)
     .then(function(resp) { return resp.json() }) //convert data to json
     .then(function(data) {
 
-        document.querySelector('.package-name').textContent = data.name;
-        document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg';
-        document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
-        document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-        console.log(1)
+        console.log(data.daily);
+        console.log(data.timezone);
+
+        let time = document.querySelectorAll('.time');
+        time.forEach(item => {
+            console.log(item);
+            item = data.daily[item];
+        })
+
+        // document.querySelector('.package-name').textContent = data.name;
+        // document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg';
+        // document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+        // document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+        // console.log(1);
     })
     .catch(function() {
 

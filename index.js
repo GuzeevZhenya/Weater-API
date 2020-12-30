ac89ea7b5892803d577172cb26bb4c5dd36dcae3
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-12-30 18:36:52 +0300

    Добавил возможность вывода табличек

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// let weatherTime = document.querySelectorAll('.weather-time');
-
-//     let city = document.querySelector('.input').value;
-//     console.log(city.value);
-
-//     weatherTime.forEach(item => {
-//         item.addEventListener('click', (e) => {
-//             let part = e.target;
-//             console.log(part.value);
-//         })
-//     })
-
-
 let time = document.querySelectorAll('.time');
 let weatherDays = document.querySelector('.weather-days');
 let city = document.querySelector('.input');
@@ -19,15 +6,22 @@ let temperature = document.querySelectorAll('.temperature');
 let button = document.querySelector('.button');
 let disclaimer = document.querySelectorAll('.disclaimer');
 let icons = document.querySelectorAll('.features li');
+let weatherInformation = document.querySelector('.pricing-table');
+weatherInformation.classList.add = '.row';
 
-let invisible = document.querySelector('.pricing-table');
+// let invisible = document.querySelector('.pricing-table');
 
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    invisible.style.display = "flex";
-    weatherDays.addEventListener('click', (e) => {
+    weatherInformation.style.display = "flex";
 
+
+    if (city.value === '') {
+        return;
+    }
+
+    weatherDays.addEventListener('click', (e) => {
         weatherDays.classList.toggle('.five-days');
         for (let i = 1; i < div.length; i++) {
             if (weatherDays.classList.contains('.five-days')) {
@@ -38,34 +32,69 @@ button.addEventListener('click', (e) => {
         }
     })
 
-    // fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=ea04db02d64d4b2b6453bfc814cd3cf9`)
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=60.99&lon=30.9&dt=1586468027&lang=ru&appid=ea04db02d64d4b2b6453bfc814cd3cf9`)
-        .then(function(resp) { return resp.json() }) //convert data to json
-        .then(function(data) {
+    fetch(`http://open.mapquestapi.com/geocoding/v1/address?key=hqZM0yzr5AMhh6Au5FZzvResHAEELg2N&location=${city.value}`)
+        .then(function (resp) {
+            return resp.json()
+        }) //convert data to json
+        .then(function (data) {
+            console.log(data.results[0].locations[0].latLng);
+            const {
+                lat,
+                lng
+            } = data.results[0].locations[0].latLng;
+            return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&dt=1586468027&lang=ru&appid=ea04db02d64d4b2b6453bfc814cd3cf9`)
 
-            for (let i = 0; i < div.length; i++) {
-                document.querySelectorAll('.package-name').textContent = city.value;
+        })
+        .then(function (resp) {
+            return resp.json()
+        })
+        .then(function (data) {
+            console.log(data);
+
+
+            for (let i = 0; i < data.hourly.length; i++) {
+
+                let hourlyDiv = document.createElement('div');
+                hourlyDiv.className = 'package featured';
+                let hourlyName = document.createElement('p');
+                hourlyName = 'package-name';
+                let hourlyHr = document.createElement('hr');
+                let hourlyTemperature = document.createElement('p');
+                hourlyTemperature.className = 'temperature';
+                let hourlyDisclaimer = document.createElement('p');
+                hourlyDisclaimer.className = 'disclaimer';
+                let hourlyTime = document.createElement('p');
+                hourlyTime.className = 'time';
+                let hourlyUl = document.createElement('ul');
+                hourlyUl.className = 'features';
+                let hourlyLi = document.createElement('li');
+
+                weatherInformation.appendChild(hourlyDiv)
+                hourlyDiv.appendChild(hourlyHr);
+                hourlyDiv.appendChild(hourlyTemperature);
+                hourlyDiv.appendChild(hourlyDisclaimer);
+                hourlyDiv.appendChild(hourlyTime);
+                hourlyDiv.appendChild(hourlyUl);
+
+
+                div[i].querySelector('.package-name').textContent = city.value;
                 temperature[i].innerHTML = Math.floor(data.hourly[i].temp - 273) + '&deg';
                 time[i].innerHTML = 'Влажность ' + data.hourly[i].humidity + "%";
                 disclaimer[i].innerHTML = data.hourly[i].weather[0]['description'];
                 icons[i].innerHTML = `<img src="https://openweathermap.org/img/wn/${data.hourly[i].weather[0].icon}@2x.png">`;
 
 
-                console.log(city.value);
+
+
 
             }
 
 
-
-            // document.querySelector('.package-name').textContent = data.name;
-            // document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg';
-            // document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
-            // document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-            // console.log(1);
         })
-        .catch(function() {
+        .catch(function () {
 
             //catch any errors
         });
-    // })
-});
+
+
+})
